f1b2792dc72734a6db68a234aab834c208f77594
Daniel Haag <danni.haag91@gmail.com>
2021-12-09 18:39:59 -0300

    primeiro commit

---
+++ PensionatoEstudantes/Estudante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PensionatoEstudantes
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int NumeroDoQuarto { get; set; }
+
+        public Estudante()
+        {
+        }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
---
+++ PensionatoEstudantes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PensionatoEstudantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nomeEstudante = string.Empty;
+            string emailEstudante = string.Empty;
+            int numeroDeQuartosAlugados = 0;
+            int numeroDoQuarto = 0;
+
+            Console.WriteLine("Quantos quartos serão alugados?");
+            numeroDeQuartosAlugados = int.Parse(Console.ReadLine());
+
+            Estudante[] quartos = new Estudante[10];
+
+            for (int i = 0; i < numeroDeQuartosAlugados; i++)
+            {
+                Console.WriteLine($"Aluguel #{i + 1}:");
+                Console.WriteLine("Nome do estudante:");
+                nomeEstudante = Console.ReadLine();
+                Console.WriteLine("E-mail do estudante:");
+                emailEstudante = Console.ReadLine();
+                Console.WriteLine("Número do quarto:");
+                numeroDoQuarto = int.Parse(Console.ReadLine());
+
+                quartos[numeroDoQuarto] = new Estudante(nomeEstudante, emailEstudante);
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < quartos.Length; i++)
+            {
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine($"{i}: {quartos[i].Nome}, {quartos[i].Email}");
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
